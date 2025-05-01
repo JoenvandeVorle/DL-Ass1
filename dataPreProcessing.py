251bdc53d6a7b697191bf69335a94127abe4e184
leon-crt <leon.pirazzoli00@gmail.com>
2025-05-01 09:54:15 +0200

    changed a couple things for clarity

--- dataPreProcessing.py
+++ dataPreProcessing.py
@@ -4,13 +4,15 @@ import scipy
 import numpy as np
 from hyperparameters import Hyperparameters as hp
 
+WINDOW_SIZE = 5 # should probably use hyperparameters class
+
 class CustomDataset(Dataset):
     def __init__(self, x, window_size):
         self.x = x # data points
         self.window_size = window_size
 
     def __getitem__(self, index):
-        # Return list of 5 input data points and the subsequent ground truth point
+        # Return list of n input data points and the subsequent ground truth point
         return torch.tensor(self.x[index : index + self.window_size]), torch.tensor(self.x[index + self.window_size + 1])
   
     def __len__(self):
@@ -24,7 +26,7 @@ laser_data = np.squeeze(data)
 
 # normalization here? 
 
-dataset = CustomDataset(laser_data, 5)
+dataset = CustomDataset(laser_data, WINDOW_SIZE)
 
 # split training and validation 80:20
 training_data, validation_data = random_split(dataset,[0.8, 0.2])
@@ -33,6 +35,7 @@ validation_data = DataLoader(validation_data, batch_size=1)
 
 print(len(training_data))
 print(len(validation_data))
-train_features, train_labels = next(iter(train_dataloader))
+
+train_features, train_label = next(iter(train_dataloader))
 print("features:" + str(train_features))
-print("labels:" + str(train_labels))
+print("labels:" + str(train_label))
