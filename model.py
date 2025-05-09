dcfbb70eec32e9f3d80f5cc04a2ca69373982c2d
Joenvandevorle <Joen@moleculi.com>
2025-05-09 17:19:24 +0200

    feedforward

--- model.py
+++ model.py
@@ -2,8 +2,11 @@ import torch
 import torch.nn as nn
 from torchinfo import summary
 
-class Model(nn.Module):
-
+class RNN_Model(nn.Module):
+    """
+    A simple RNN model for time series prediction.
+    """
+    modelname = "RNN"
     input_size: int
     hidden_size: int
     window_size: int
@@ -11,7 +14,7 @@ class Model(nn.Module):
     activation_function: nn.Module
 
     def __init__(self, input_size: int, hidden_size: int, window_size: int, num_layers: int):
-        super(Model, self).__init__()
+        super(RNN_Model, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.window_size = window_size
@@ -32,3 +35,54 @@ class Model(nn.Module):
 
     def display(self):
         summary(self, self.window_size)
+
+    def init_weights(self):
+        for name, param in self.named_parameters():
+            if 'weight' in name:
+                nn.init.xavier_uniform_(param.data)
+            elif 'bias' in name:
+                nn.init.zeros_(param.data)
+
+
+class FeedForwardModel(nn.Module):
+    """
+    A simple feedforward model for time series prediction.
+
+    It should take in a series of time steps and output a prediction for the next time step.
+    """
+    modelname = "FeedForward"
+    input_size: int
+    hidden_size: int
+    num_layers: int
+
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int):
+        super(FeedForwardModel, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        layers = []
+        layers.append(nn.Linear(input_size, hidden_size))
+        for _ in range(num_layers - 1):
+            layers.append(nn.Linear(hidden_size, hidden_size))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(hidden_size, input_size))
+        self.model = nn.Sequential(*layers)
+
+        self.output = nn.Linear(input_size, 1)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.model(x)
+        x = self.output(x)
+        return x
+
+    def display(self):
+        summary(self, self.input_size)
+
+    def init_weights(self):
+        for name, param in self.named_parameters():
+            if 'weight' in name:
+                nn.init.xavier_uniform_(param.data)
+            elif 'bias' in name:
+                nn.init.zeros_(param.data)
